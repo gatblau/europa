7825691cbbac460bb4b85ca294f0cffc61c3e170
gatblau <no-reply@gatblau.org>
2015-12-04 15:24:21 +0000

    Outputs coloured text in Darwin but not in Cygwin.

--- europa.sh
+++ europa.sh
@@ -18,7 +18,15 @@
 #
 
 # define output colours
-RED='\033[0;31m'; GREEN='\033[0;32m'; CYAN='\033[0;36m'; YELLOW='\033[0;43m'; NC='\033[0m'
+RED='\033[0;31m'; GREEN='\033[0;32m'; CYAN='\033[0;36m'; YELLOW='\033[0;33m'; NC='\033[0m'
+
+out() {
+   if [[ $1 == CYGWIN* ]]; then
+      echo $3
+   else
+      echo $2$3"${NC}"
+   fi
+}
 
 # parameter $1: name of the packer file to download
 check_packer() {
@@ -44,89 +52,88 @@ check_vm () {
    fi
 }
 
-# reset terminal colour
-echo '${NC}'
-
 os="$(uname)"
+
+# reset terminal colour
+out $os "${NC}"
 
 startTime=$(date -u +"%s")
 
-echo "${CYAN}downloading build scripts...${NC}"
+out $os "${CYAN}" "downloading build scripts..."
 git clone https://github.com/gatblau/europa.git
 
-echo "${CYAN}determining the latest version...${NC}"
+out $os "${CYAN}" "determining the latest version..."
 cd europa
 if [[ -z "$1" ]]; then
-    echo "${CYAN}reading version from latest tag...${NC}"
+    out $os "${CYAN}" "reading version from latest tag..."
     tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 else
-    echo "${CYAN}using specified tag ${YELLOW}${1}${CYAN} as version...${NC}"
+    out $os "${CYAN}" "using version tag:"
+    out $os "${YELLOW}" ">>>>   ${1}   <<<<"
     tag="$1"
 fi
 if [[ -e 'build/roles/europa/files/shell/version' ]]; then
    rm 'build/roles/europa/files/shell/version'
 fi
 echo $tag >> build/roles/europa/files/shell/version
-echo "${CYAN}version is ${YELLOW}$tag${CYAN}..."
 
-echo "${CYAN}switching to latest version...${NC}"
+out $os "${CYAN}" "switching to latest version..."
 git checkout $tag
 
-echo "${CYAN}dowloading Europa packages, please wait...${NC}"
+out $os "${CYAN}" "dowloading Europa packages, please wait..."
 cd build
 sh fetch.sh
 cd ..
 
 if [[ $os == CYGWIN* ]]; then
-    echo "${CYAN}Windows installation identified.${NC}"
+    out $os "${CYAN}" "getting packer for WINDOWS build..."
     packer_zip='packer_0.8.6_windows_amd64.zip'
     appliance_folder='c:/appliances'
     packer_exe='packer.exe'
 elif [[ $os == Darwin* ]]; then
-    echo "${CYAN}Darwin installation identified.${NC}"
+    out $os "${CYAN}" "getting packer for DARWIN build..."
     packer_zip='packer_0.8.6_darwin_386.zip'
     appliance_folder='~/appliances'
     packer_exe='packer'
 else
-    echo "${RED}Installation on ${os} is not supported!"
-    echo "cannot continue...${NC}"
+    out $os "${RED}" "Installation on ${os} is not supported!, cannot continue..."
     exit
 fi
 
 check_packer $packer_zip $packer_exe
 
 if check_vm europa; then
-    echo "${CYAN}Un-versioned Europa VM found in Virtual Box, deleting it...${NC}"
+    out $os "${CYAN}" "Europa build image found in Virtual Box, deleting it..."
     VBoxManage unregistervm europa --delete
 fi
 
-echo "${CYAN}building the Europa appliance, please wait...${NC}"
+out $os "${CYAN}" "building the Europa appliance, please wait..."
 ./packer_files/"$packer_exe" build europa.vbox.json
 
 if [[ ! -d $appliance_folder ]]; then
-    echo "${CYAN}creating the ${appliance_folder} folder...${NC}"
+    out $os "${CYAN}" "creating the ${appliance_folder} folder..."
     mkdir -p $appliance_folder
 fi
 
-echo "${CYAN}copying the Europa virtual appliance to ${YELLOW}${appliance_folder}${NC} ..."
-echo "${CYAN}future imports can use this file."
+out $os "${CYAN}" "copying the Europa virtual appliance to ${appliance_folder}..."
+out $os "${YELLOW}" "The appliance can be imported as many times as required from this folder."
 cp -v europa-vbox/europa.ova $appliance_folder/europa_$tag.ova
 
-echo "${CYAN}importing the Europa appliance into Virtual Box, please wait...${NC}"
+out $os "${YELLOW}" "importing the Europa appliance into Virtual Box, please wait..."
 VBoxManage import $appliance_folder/europa_$tag.ova --vsys 0 --vmname europa_$tag
 
 read -n1 -p "Do you want to delete the installation files? [Y-N]" deleteFiles
 case $deleteFiles in
     [Yy]* ) cd .. && rm -rf * ;;
-    * ) echo "${CYAN}installation files can be found at $PWD${NC}" ;;
+    * ) out $os "${CYAN}" "installation files can be found at $PWD" ;;
 esac
 
 endTime=$(date -u +"%s")
 diff=$(($endTime-$startTime))
 
-echo "${YELLOW}Europa build process complete: it took $(($diff / 60)) minutes and $(($diff % 60)) seconds.${NC}"
+out $os "${YELLOW}" "Europa build process complete: it took $(($diff / 60)) minutes and $(($diff % 60)) seconds."
 
-echo "${CYAN}Launching Virtual Box...${NC}"
+out $os "${CYAN}" "Launching Virtual Box..."
 
 (VirtualBox &)
 
