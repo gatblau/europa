495e6038094e35032762aea84ace39e8e943e373
gatblau <no-reply@gatblau.org>
2016-02-05 17:00:22 +0000

    Leave display on so that the screensaver can kick in.

--- build/postconfig.sh
+++ build/postconfig.sh
@@ -28,3 +28,5 @@ id="${id#\'}"
 sudo -u europa dbus-launch gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:"$id"/ login-shell true
 # sets the terminal not to use theme colours
 sudo -u europa dbus-launch gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:"$id"/ use-theme-colors false
+# leave the display turned on - so the screensaver can kick in
+sudo -u europa dbus-launch gsettings set org.gnome.desktop.session idle-delay 0
