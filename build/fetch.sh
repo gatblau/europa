ebbc159d5ca640e123c09ddd693402e1795f7fef
gatblau <no-reply@gatblau.org>
2016-02-06 22:17:20 +0000

    Installs IntelliJ Community Edition.

--- build/fetch.sh
+++ build/fetch.sh
@@ -50,6 +50,7 @@ download "http://opensource.wandisco.com/centos/7/git/x86_64/" "perl-Git-2.7.0-1
 download "http://dl.bintray.com/groovy/maven/" "apache-groovy-binary-2.4.5.zip"
 download "https://releases.hashicorp.com/vagrant/1.8.1/" "vagrant_1.8.1_x86_64.rpm"
 download "https://d1opms6zj7jotq.cloudfront.net/idea/" "ideaIU-15.0.3.tar.gz"
+download "https://d1opms6zj7jotq.cloudfront.net/idea/" "ideaIC-15.0.3.tar.gz"
 download "http://downloads.typesafe.com/scalaide-pack/4.3.0-vfinal-luna-211-20151201/" "scala-SDK-4.3.0-vfinal-2.11-linux.gtk.x86_64.tar.gz"
 download "http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/mars/R/" "eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz"
 download "https://bitbucket.org/rhiokim/haroopad-download/downloads/" "haroopad-v0.13.1-x64.tar.gz"
