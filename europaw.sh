872dee6e34c6bdb75fe7706fb6fca0722db8665d
gatblau <no-reply@gatblau.org>
2016-07-03 20:41:26 +0100

    upgrades to virtualbox 5.0.24

--- europaw.sh
+++ europaw.sh
@@ -78,7 +78,7 @@ fi
 if [[ ! -d "C:\Program Files\Oracle\VirtualBox" ]]; then
     if [[ ! -e vbox.exe ]]; then
         echo 'downloading Virtual Box'
-        wget -O vbox.exe 'http://download.virtualbox.org/virtualbox/5.0.14/VirtualBox-5.0.14-105127-Win.exe'
+        wget -O vbox.exe 'http://download.virtualbox.org/virtualbox/5.0.24/VirtualBox-5.0.24-108355-Win.exe'
         chmod +x vbox.exe
     fi
     echo 'installing Virtual Box, please wait!'
