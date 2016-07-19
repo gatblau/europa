7a13ec13b66d21e8dfcc5527a2d8c1258404183c
gatblau <no-reply@gatblau.org>
2016-07-19 12:52:27 +0100

    improve comments

--- build/roles/europa/files/openshift/os-cleanup.sh
+++ build/roles/europa/files/openshift/os-cleanup.sh
@@ -2,14 +2,18 @@
 #
 # Cleans up an existing OpenShift instance.
 #
-# stops the service
+
+# stops the Openshift service
 sudo systemctl stop openshift
-# removes config folders
+
+# removes the Openshift configuration folders
 sudo rm -rf /usr/local/openshift/default/openshift.local.*
-# removes the kubernetes docker containers
+
+# removes the all kubernetes docker containers runnig on the host
 id=$(docker ps -af name=k8s_* -q)
 if [[ ! -z $id ]]; then
    docker rm -f $id
 fi
-# removes kube config
+
+# removes kube config folder for user
 sudo rm -rf ~/.kube
