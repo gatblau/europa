4b323e3b1ea151bcef6c069362bdd5c230b708fe
gatblau <no-reply@gatblau.org>
2016-07-19 12:53:27 +0100

    create secret for TLS certs

--- build/roles/europa/files/openshift/os-init.sh
+++ build/roles/europa/files/openshift/os-init.sh
@@ -2,34 +2,57 @@
 #
 # Creates the basic services on a fresh OpenShift instance
 #
-KUBE=/home/europa/.kube
-OS_HOME=/usr/local/openshift/default
-CONF_DIR=$OS_HOME/openshift.local.config/master
 
-PATH=$PATH:$OS_HOME
+PATH=$PATH:/usr/local/openshift/default
 
-# creates the integrated docker registry
-oc create serviceaccount registry -n defaults
+# =========================================================================
+# INTEGRATED DOCKER REGISTRY
+# =========================================================================
+
+# creates the registry service account and grant access to the Privileged Security Context Constraints (SCC)
 oadm policy add-scc-to-user privileged system:serviceaccount:default:registry
+
+# creates the integrated registry
 oadm registry \
     --service-account=registry \
-    --tls-certificate=$KUBE/openshift-registry.crt \
-    --tls-key=$KUBE/openshift-registry.key
+    --mount-host=/usr/local/openshift/default/registry
+
+# create the secret for the registry TLS certificates
+oc secrets new registry-secret \
+    /etc/secrets/openshift-registry.crt \
+    /etc/secrets/openshift-registry.key
+
+# add the secret to the registry pod’s service accounts registry and default
+oc secrets add serviceaccounts/registry secrets/registry-secret
+oc secrets add serviceaccounts/default secrets/registry-secret
+
+# add the secret volume to the registry deployment configuration
+oc volume dc/docker-registry --add --type=secret --secret-name=registry-secret -m /etc/secrets
 
 # ﻿Enable TLS﻿ on the registry
-#oc env dc/docker-registry \
-#    REGISTRY_HTTP_TLS_CERTIFICATE=$CONF_DIR/openshift-registry.crt \
-#    REGISTRY_HTTP_TLS_KEY=$CONF_DIR/openshift-registry.key
+oc env dc/docker-registry \
+    REGISTRY_HTTP_TLS_CERTIFICATE=/etc/secrets/openshift-registry.crt \
+    REGISTRY_HTTP_TLS_KEY=/etc/secrets/openshift-registry.key
 
-# needs to copy certs to secrets folder
-#    /etc/secrets/registry.crt
-#    /etc/secrets/registry.key
+# Update the scheme used for the registry’s readiness probe from HTTP to HTTPS
+oc patch dc/docker-registry --api-version=v1 -p '{"spec": {"template": {"spec": {"containers":[{
+    "name":"registry",
+    "readinessProbe":  {"httpGet": {"scheme":"HTTPS"}}
+  }]}}}}'
+
+# =========================================================================
+# ROUTER
+# =========================================================================
 
 # allows the ﻿service account "router" to access the host network on nodes
 oadm policy add-scc-to-user hostnetwork -z router
 
 # creates the router
 oadm router
+
+# =========================================================================
+# TEMPLATES
+# =========================================================================
 
 URI=https://raw.githubusercontent.com/openshift/origin/master/examples
 
