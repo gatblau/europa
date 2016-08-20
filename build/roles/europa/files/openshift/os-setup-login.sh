247cbf888bde42d2d5e956d0fa97f14e6683b79d
gatblau <info@gatblau.org>
2016-08-20 15:24:35 +0100

    Makes sure that the current user is configured.

--- build/roles/europa/files/openshift/os-setup-login.sh
+++ build/roles/europa/files/openshift/os-setup-login.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 #
-# Sets up login as administrator for europa and root users
+# Sets up login as administrator for the current user.
 #
-usr_home=/home/europa
 os_home=/usr/local/openshift/default
 
 # re-creates the configuration folder
-sudo rm -rf $usr_home/.kube
-sudo mkdir $usr_home/.kube
-
-sudo rm -rf /root/.kube
-sudo mkdir /root/.kube
+rm -rf ~/.kube
+mkdir ~/.kube
 
 # copy the kube config file
-sudo cp $os_home/openshift.local.config/master/admin.kubeconfig /root/.kube/config
-sudo cp $os_home/openshift.local.config/master/admin.kubeconfig $usr_home/.kube/config
-
-# assigning permissions and user/group
-sudo chmod 0777 $usr_home/.kube/config
-sudo chgrp europa $usr_home/.kube/config
-sudo chown europa $usr_home/.kube/config
+cp $os_home/openshift.local.config/master/admin.kubeconfig /root/.kube/config
