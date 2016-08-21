31065c759b6883f08657cc80fbf15ab711cc15e2
gatblau <no-reply@gatblau.org>
2016-08-21 09:15:44 +0100

    removes wrong policy command.

--- build/roles/europa/files/openshift/os-create-router.sh
+++ build/roles/europa/files/openshift/os-create-router.sh
@@ -7,9 +7,6 @@
 OS_HOME=/usr/local/openshift/default
 PATH=$PATH:$OS_HOME
 
-echo 'Granting the registry service account access to the Privileged Security Context Constraints (SCC)'
-oadm policy add-scc-to-user privileged system:serviceaccount:default:registry
-
 echo 'Allowing the ﻿service account router to access the host network on nodes'
 oadm policy add-scc-to-user hostnetwork -z router
 
