18bdc5e30bddd8ea8dd2393b1fe40034532c5af1
gatblau <info@gatblau.org>
2016-10-16 13:41:15 +0100

    renames cip function.

---
+++ build/roles/europa/files/shell/c.sh
@@ -0,0 +1,23 @@
+function c() {
+    if [[ "$1" == "--help" ]]; then
+		___print_c_usage
+	else
+		case "$1" in
+			ip) ___c_ip ;;
+			*) ___print_c_usage ;;
+		esac
+	fi
+}
+
+function ___c_ip() {
+    if [ "$1" == "?" ] || [ -z "$1" ]; then
+        ___print_c_usage
+    else
+        docker inspect --format '{{.NetworkSettings.IPAddress}}' $1
+    fi
+}
+
+function ___print_c_usage() {
+    echo -e "${GREEN}Usage:"
+	echo -e "${CYAN}  c ip <container name>: ${GREEN} shows the IP of the named container.${NC}"
+}
