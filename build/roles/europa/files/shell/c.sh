ad398ae5af2682c9ae08c9c871e49ec4ee7ecfae
gatblau <no-reply@gatblau.org>
2016-10-19 20:09:02 +0100

    adds vars function.

--- build/roles/europa/files/shell/c.sh
+++ build/roles/europa/files/shell/c.sh
@@ -4,6 +4,7 @@ function c() {
 	else
 		case "$1" in
 			ip) ___c_ip $2 ;;
+			vars) ___c_vars $2 ;;
 			*) ___print_c_usage ;;
 		esac
 	fi
@@ -17,7 +18,16 @@ function ___c_ip() {
     fi
 }
 
+function ___c_vars() {
+    if [ "$1" == "?" ] || [ -z "$1" ]; then
+        ___print_c_usage
+    else
+        docker inspect --format '{{ index (index .Config.Env) 0 }}' $1
+    fi
+}
+
 function ___print_c_usage() {
     echo -e "${GREEN}Usage:"
 	echo -e "${CYAN}  c ip <container name>: ${GREEN} shows the IP of the named container.${NC}"
+	echo -e "${CYAN}  c vars <container name>: ${GREEN} shows the environment variables in the named container.${NC}"
 }
