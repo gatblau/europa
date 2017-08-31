9e249c2a068c0ae8694d26b021c2b5c5da19b877
gatblau <no-reply@gatblau.org>
2017-08-31 08:19:05 +0100

    improves openshift deployment on single VM

--- build/roles/origin-tenant-htpasswd/files/new-user.sh
+++ build/roles/origin-tenant-htpasswd/files/new-user.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # new-user.sh creates a new user / pwd combination in htpasswd file and add-user-to-role under a specified namespace
-# usage: sh new-user.sh 4 /usr/local/openshift basic-user myproject-dev y
+# usage: sh new-user.sh 4 /usr/local/openshift myuser myproject-dev y
 #
 # input vars:
 #   $1 : password len
@@ -17,14 +17,14 @@ pwd=$(perl -le 'print map { (a..z,A..Z,0..9)[rand 62] } 0..pop' $1)
 htpasswd -c -b $2'/users.htpasswd' $3 $pwd
 
 # is the user an admin or a basic user?
-if [ "$5" eq "y" ]; then
+if [ "$5" == "y" ]; then
     role='admin'
 else
     role='basic-user'
 fi
 
 # add user to role basic-user
-$2/oc add-role-to-user basic-user $role $3 -n $4 --config=$2/openshift.local.config/master/admin.kubeconfig
+$2/oc adm policy add-role-to-user basic-user $role $3 -n $4 --config=$2/openshift.local.config/master/admin.kubeconfig
 
 # writes the user and pwd to a file
 echo "$3 - $pwd" >> "$2/user-info"
